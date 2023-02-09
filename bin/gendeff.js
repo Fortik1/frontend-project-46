0b43f17002d32aa15972949bb9452402851edf1e
Даниил <daniil@MacBook-Pro-konstantin-2.local>
2023-02-09 10:52:10 +0300

    Mod bin/gendeff.js

--- bin/gendeff.js
+++ bin/gendeff.js
@@ -3,7 +3,9 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
+    .arguments('<filepath1> <filepath2>')
     .description('Compares two configuration files and shows a difference.')
     .version('0.1.0')
+    .option('-f, --format <type>', 'output format')
 
 program.parse();
