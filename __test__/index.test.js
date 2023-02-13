eb6966dbda8089c868eed1d7412a45c41779da73
Даниил <daniil@MacBook-Pro-konstantin-2.local>
2023-02-13 14:35:47 +0300

    fix package-lock.json, package.json and index.test.js

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,12 +1,12 @@
 import url from 'url';
 import { test, expect } from '@jest/globals';
 import { dirname } from 'node:path';
-import gendiff from '../src/index.js';
 import fs from 'fs';
 import path from 'path';
+import gendiff from '../src/index.js';
 
-const __fileName = url.fileURLToPath(import.meta.url);
-const __dirname = dirname(__fileName);
+const fileName = url.fileURLToPath(import.meta.url);
+const __dirname = dirname(fileName);
 
 const getFixturePath = (nameFile) => path.join(__dirname, '..', 'files', nameFile);
 const readFile = (nameFile) => fs.readFileSync(getFixturePath(nameFile), 'utf-8');
@@ -17,5 +17,5 @@ const file1 = './files/file1.json';
 const file2 = './files/file2.json';
 
 test('test1', () => {
-    expect(gendiff(file1, file2)).toEqual(test1Default);
-})
+  expect(gendiff(file1, file2)).toEqual(test1Default);
+});
