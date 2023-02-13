7a123f8152f76192f9b08f344598a06e4e74f4b7
Даниил <daniil@MacBook-Pro-konstantin-2.local>
2023-02-13 13:16:25 +0300

    fix all files "eslint"

--- src/format/formatMain.js
+++ src/format/formatMain.js
@@ -1,8 +1,8 @@
 import stylishFormat from './stylish.js';
 
 export default function formatter(tree, format) {
-    switch (format) {
-        case 'stylish':
-            return stylishFormat(tree);
-    }
+  switch (format) {
+    case 'stylish':
+      return stylishFormat(tree);
+  }
 }
--- src/getTree.js
+++ src/getTree.js
@@ -1,44 +1,44 @@
 import _ from 'lodash';
 
 export default function getTree(data1, data2) {
-    const files = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
+  const files = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
 
-    const result = files.map((key) => {
-        if (!Object.hasOwn(data2, key)) {
-            return {
-                key,
-                type: 'deleted',
-                value: data1[key],
-            }
-        }
-        if (!Object.hasOwn(data1, key)) {
-            return {
-                key,
-                type: 'added',
-                value: data2[key],
-            }
-        }
-        if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-            return {
-                key,
-                children: getTree(data1[key], data2[key]),
-                type: 'nested',
-            }
-        }
-        if (!_.isEqual(data1[key], data2[key])) {
-            return {
-                key,
-                type: 'changed',
-                value1: data1[key],
-                value2: data2[key],
-            }
-        }
-        return {
-            key,
-            type: 'unchanged',
-            value: data1[key],
-        }
-    })
+  const result = files.map((key) => {
+    if (!Object.hasOwn(data2, key)) {
+      return {
+        key,
+        type: 'deleted',
+        value: data1[key],
+      };
+    }
+    if (!Object.hasOwn(data1, key)) {
+      return {
+        key,
+        type: 'added',
+        value: data2[key],
+      };
+    }
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      return {
+        key,
+        children: getTree(data1[key], data2[key]),
+        type: 'nested',
+      };
+    }
+    if (!_.isEqual(data1[key], data2[key])) {
+      return {
+        key,
+        type: 'changed',
+        value1: data1[key],
+        value2: data2[key],
+      };
+    }
+    return {
+      key,
+      type: 'unchanged',
+      value: data1[key],
+    };
+  });
 
-    return result;
+  return result;
 }
