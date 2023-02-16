1ade40a2f9dd73c54b3db5e618a17ea97c098887
Даниил <daniil@MacBook-Pro-konstantin-2.local>
2023-02-16 10:39:18 +0300

    mod formatMain and stylish

--- src/format/formatMain.js
+++ src/format/formatMain.js
@@ -1,9 +1,12 @@
 import stylishFormat from './stylish.js';
+import plainFormat from './plain.js';
 
 function formatter(tree, format) {
   switch (format) {
     case 'stylish':
       return stylishFormat(tree);
+    case 'plain':
+      return plainFormat(tree);
     default:
       throw new Error(
         `The ${format} format is supported.\n supported fornmats: stylish, plain, json`,
